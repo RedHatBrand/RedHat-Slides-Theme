82aa21ece5cebc1f174cbce49d6bd72b5cc3d39d
Andy Fitzsimon <andyfitz@gmail.com>
2014-05-26 21:28:46 +1000

    prepping for svg's
    svg prep with svg min.  new gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var stylus = require('gulp-stylus');
 var nib = require('nib');
 var prefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
-
+var svgmin = require('gulp-svgmin');
 
 // Get and render all .styl files recursively
 gulp.task('default', function () {
@@ -13,4 +13,11 @@ gulp.task('default', function () {
     .pipe(prefix())
     //.pipe(minifyCSS())
     .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('svg', function() {
+    return gulp.src('.source/svg/**/*.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('./dist'));
 });

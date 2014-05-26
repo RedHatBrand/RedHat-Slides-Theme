c36bfb503005bb98621f75d3f91a78942f2255db
Brad Parker <hi@bradpaker.com>
2014-05-26 12:38:34 +1000

    Adds nib and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var nib = require('nib');
+var prefix = require('gulp-autoprefixer');
 var minifyCSS = require('gulp-minify-css');
+
 
 // Get and render all .styl files recursively
 gulp.task('default', function () {
   gulp.src('./source/**/*.styl')
-    .pipe(stylus())
+    .pipe(stylus({use: [nib()]}))
+    .pipe(prefix())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./dist'));
 });
